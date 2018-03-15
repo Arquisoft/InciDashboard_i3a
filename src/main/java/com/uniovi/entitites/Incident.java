fe61345df9dcd226f8dc686ecce605732e2239bd
eleallegue <elenalleglez@gmail.com>
2018-03-15 13:18:54 +0100

    added operator and changes in incident fields

--- src/main/java/com/uniovi/entitites/Incident.java
+++ src/main/java/com/uniovi/entitites/Incident.java
@@ -1,34 +1,38 @@
 package com.uniovi.entitites;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import java.util.List;
+import java.util.Map;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Entity;
 
 @Entity
 public class Incident {
 
-	@Id
-	@GeneratedValue
 	private long id;
-
 	private String name;
 	private String description;
 	private IncidentStates state;
 	private String location;
-	private String[] tags;
+	private List<String> tags;
+	private List<String> multimedia;
+	private Map<String, String> property_value;
+	private List<String> comments;
 
 	public Incident() {
 
 	}
 
-	public Incident(String name, String description, IncidentStates state, String location, String[] tags) {
+	public Incident(Long id, String name, String description, IncidentStates state, String location, List<String> tags,
+			List<String> multimedia, Map<String, String> property_value, List<String> comments) {
 		super();
 		this.name = name;
 		this.description = description;
 		this.state = state;
 		this.location = location;
 		this.tags = tags;
+		this.multimedia = multimedia;
+		this.property_value = property_value;
+		this.comments = comments;
 	}
 
 	public String getName() {
@@ -63,12 +67,44 @@ public class Incident {
 		this.location = location;
 	}
 
-	public String[] getTags() {
+	public List<String> getTags() {
 		return tags;
 	}
 
-	public void setTags(String[] tags) {
+	public void setTags(List<String> tags) {
 		this.tags = tags;
+	}
+
+	public List<String> getMultimedia() {
+		return multimedia;
+	}
+
+	public void setMultimedia(List<String> multimedia) {
+		this.multimedia = multimedia;
+	}
+
+	public Map<String, String> getProperty_value() {
+		return property_value;
+	}
+
+	public void setProperty_value(Map<String, String> property_value) {
+		this.property_value = property_value;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public List<String> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<String> comments) {
+		this.comments = comments;
 	}
 
 }
