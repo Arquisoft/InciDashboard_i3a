7e7e3519d473a023337e99e8c42c55c62b1fe7d3
ana <uo251547@uniovi.es>
2018-03-21 17:46:30 +0100

    added incident controller

---
+++ src/main/java/com/uniovi/controllers/IncidentController.java
@@ -0,0 +1,30 @@
+package com.uniovi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.uniovi.entitites.Incident;
+import com.uniovi.services.IncidentsService;
+
+@Controller
+public class IncidentController {
+	@Autowired
+	private IncidentsService incidentsService;
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String getLogin() {
+		return "login";
+	}
+
+	@RequestMapping(value = "/incidents", method = RequestMethod.GET)
+	public String getDashboard(Model model) {
+		List<Incident> incidents = incidentsService.getAllIncidents();
+		model.addAttribute("incidentsList", incidents);
+		return "incidents";
+	}
+}
