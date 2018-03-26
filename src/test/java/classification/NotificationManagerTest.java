8b77c8bac7eda8a8c35976a3a4cdcaa1cd57c009
eleallegue <elenalleglez@gmail.com>
2018-03-26 20:57:07 +0200

    simple motification manager test

---
+++ src/test/java/classification/NotificationManagerTest.java
@@ -0,0 +1,30 @@
+package classification;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import com.uniovi.clasification.NotificationManager;
+import com.uniovi.entitites.Incident;
+import com.uniovi.entitites.IncidentStates;
+import com.uniovi.entitites.Notification;
+import com.uniovi.entitites.Operator;
+
+public class NotificationManagerTest {
+
+	@Test
+	public void testInserting() {
+		Operator o = new Operator("pepe", "123456");
+		Incident i = new Incident("Incidente de prueba", "Pruebaaa", IncidentStates.OPEN, "41N56E", new ArrayList<>(),
+				new ArrayList<>(), new HashMap<>());
+		Notification n = new Notification(i, o);
+
+		NotificationManager manager = NotificationManager.getInstance();
+		assertTrue(manager.getNotifications().isEmpty());
+		manager.addNotification(n);
+		assertTrue(manager.getNotifications().size() == 1);
+	}
+}
