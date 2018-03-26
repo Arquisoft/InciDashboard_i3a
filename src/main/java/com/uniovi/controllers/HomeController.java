e1a13d7f255b0af26dbb6d8860747b150c72d8aa
Víctor Suárez Fernández <vicsufer@gmail.com>
2018-03-26 09:20:47 +0200

    Now it runs

--- src/main/java/com/uniovi/controllers/HomeController.java
+++ src/main/java/com/uniovi/controllers/HomeController.java
@@ -1,14 +1,10 @@
 package com.uniovi.controllers;
-
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-	private static final Logger logger = Logger.getLogger(HomeController.class);
 
 	@RequestMapping("/")
 	public String getHome(Model model) {
