13b407dc67a4f2bce452b90614042f19dfd2ee48
eleallegue <elenalleglez@gmail.com>
2018-03-26 20:11:05 +0200

    tests of incident, operator and properties added

--- src/main/java/com/uniovi/properties/Wind.java
+++ src/main/java/com/uniovi/properties/Wind.java
@@ -1,14 +1,21 @@
 package com.uniovi.properties;
 
-public class Wind implements Property{
+public class Wind implements Property {
 
 	private static final double MAX_VALUE = 30.0;
 	private Double value;
-	
+
 	public Wind(Double value) {
+		checkValue(value);
 		this.value = value;
 	}
-	
+
+	private void checkValue(double val) {
+		if (val < 0) {
+			throw new IllegalArgumentException("The wind's speed should have a value greater or equal to 0");
+		}
+	}
+
 	@Override
 	public boolean hasNormalValue() {
 		if (this.value > MAX_VALUE)
