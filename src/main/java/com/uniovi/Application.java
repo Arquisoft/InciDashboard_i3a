7d0fbb5772dd937e5ee7a53ec2bd57cea032119c
eleallegue <elenalleglez@gmail.com>
2018-04-12 12:08:23 +0200

    changes

--- src/main/java/com/uniovi/Application.java
+++ src/main/java/com/uniovi/Application.java
@@ -1,13 +1,8 @@
 package com.uniovi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class })
 @EnableMongoRepositories("com.uniovi.repository")
 public class Application {
 
--- src/test/java/controllers/OperatorControllerTest.java
+++ src/test/java/controllers/OperatorControllerTest.java
@@ -1,45 +1,65 @@
 package controllers;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.net.URL;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.uniovi.Application;
 
 @SpringBootTest(classes = { Application.class })
 @RunWith(SpringJUnit4ClassRunner.class)
-@DirtiesContext
+@ActiveProfiles("test")
 public class OperatorControllerTest {
 
-	@InjectMocks
-	private OperatorControllerTest operatorController;
+	@Value("${local.server.port}")
+	private int port;
+
+	private URL base;
+	private RestTemplate template;
+
+	// @InjectMocks
+	// private OperatorControllerTest operatorController;
 
 	private MockMvc mockMvc;
 
+	@Autowired
+	private WebApplicationContext context;
+
 	@Before
 	public void setUp() throws Exception {
+		System.err.println(port);
+		this.base = new URL("http://localhost:" + port + "/");
+		template = new RestTemplate();
 		MockitoAnnotations.initMocks(this);
-		this.mockMvc = MockMvcBuilders.standaloneSetup(operatorController).build();
+		this.mockMvc = MockMvcBuilders.standaloneSetup(context).build();
 	}
 
 	@Test
-	public void testNotLoggedIn() throws Exception {
-		MockHttpServletRequestBuilder request = get("/operator/list");
-		int status = mockMvc.perform(request).andExpect(redirectedUrl("/login")).andReturn().getResponse().getStatus();
-		assertEquals(HttpStatus.FOUND.value(), status);
-	}
+	public void testLoginPage() throws Exception {
+		template.getForEntity(base.toString(), String.class);
+		String message = mockMvc.perform(get("/login")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Username:")))
+				.andExpect(content().string(containsString("Password:")))
+				.andExpect(content().string(containsString("Kind:"))).andReturn().toString();
 
+		assertNotNull(message);
+	}
 }
