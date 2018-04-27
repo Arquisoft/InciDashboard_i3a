32f79be9269f253c480169de2f33ec78d30e76de
eleallegue <elenalleglez@gmail.com>
2018-04-27 13:37:22 +0200

    creation of the test classes for entity testing

---
+++ src/test/java/entities/CommentTest.java
@@ -0,0 +1,24 @@
+package entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommentTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/entities/OperatorTest.java
@@ -0,0 +1,23 @@
+package entities;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OperatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
