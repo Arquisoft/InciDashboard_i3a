385fdf2b611386c0387b66816aa7c9ed49258540
eleallegue <elenalleglez@gmail.com>
2018-05-07 21:13:40 +0200

    new tests

--- src/test/java/io/github/asw/i3a/operatorsWebClient/entities/CommentTest.java
+++ src/test/java/io/github/asw/i3a/operatorsWebClient/entities/CommentTest.java
@@ -1,6 +1,9 @@
 package io.github.asw.i3a.operatorsWebClient.entities;
 
+import static org.junit.Assert.assertTrue;
 import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
+
+import java.util.Date;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -16,6 +19,13 @@ public class CommentTest {
 	public void allPropertiesTest() {
 		assertPojoMethodsFor(Comment.class).testing(Method.GETTER, Method.SETTER, Method.EQUALS, Method.HASH_CODE,
 				Method.TO_STRING, Method.CONSTRUCTOR).areWellImplemented();
+	}
+
+	@Test
+	public void extraTest() {
+		Comment c = new Comment("hola", "idOp");
+		Date d = new Date();
+		assertTrue(c.getDate().getTime() <= d.getTime());
 	}
 
 }
--- src/test/java/io/github/asw/i3a/operatorsWebClient/entities/OperatorTest.java
+++ src/test/java/io/github/asw/i3a/operatorsWebClient/entities/OperatorTest.java
@@ -1,5 +1,6 @@
 package io.github.asw.i3a.operatorsWebClient.entities;
 
+import static org.junit.Assert.assertTrue;
 import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
 
 import org.junit.Test;
@@ -16,6 +17,13 @@ public class OperatorTest {
 	public void allPropertiesTest() {
 		assertPojoMethodsFor(Operator.class).testing(Method.GETTER, Method.SETTER, Method.EQUALS, Method.HASH_CODE,
 				Method.TO_STRING, Method.CONSTRUCTOR).areWellImplemented();
+	}
+
+	@Test
+	public void extraTest() {
+		Operator op = new Operator("Pepe@gmail.com", "123456");
+		assertTrue(op.getEmail().equals("Pepe@gmail.com"));
+		assertTrue(op.getPassword().equals("123456"));
 	}
 
 }
